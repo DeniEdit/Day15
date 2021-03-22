09a6a01b8cc0d8521274d959a3becdd22b645633
DeniEdit <milicevic.deni@gmail.com>
2021-03-22 14:01:07 +0100

    Exercise 08

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,5 +1,6 @@
-function sum(num1, num2) {
-     return sum = num1 + num2;
+function sum(num1, num2 = 1) {
+     return num1 + num2;
 }
 console.log(sum(4, 6));
+console.log(sum(3));
 module.exports = sum;
